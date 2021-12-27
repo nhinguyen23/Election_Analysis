3528f36d2ade8d9da84ba86b30e97e156777f69d
Nhi <nhsoria40@gmail.com>
2021-12-26 21:53:16 -0800

    Adding the election analysis.

--- Python_practice.py
+++
@@ -1,9 +0,0 @@
-#Import the datetime class from the datetime module.
-import datetime
-#Use the now() attribute on the datetime class to get the present time.
-now = datetime.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
-
-import os
-dir(os)
